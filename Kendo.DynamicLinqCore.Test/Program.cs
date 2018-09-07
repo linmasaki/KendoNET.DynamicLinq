49e996b5b019ae7950c623e56f2b7c4cb162a35a
JFTCoCo <coco@jfetek.com>
2018-09-07 22:50:11 +0800

    Update project structure.

--- Kendo.DynamicLinqCore.Test/Program.cs
+++ Kendo.DynamicLinqCore.Test/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Linq;
 using System.Runtime.Serialization;
-using Kendo.DynamicLinqCore;
 
-namespace Kendo.DynamicLinq.ConsoleTests
+namespace Kendo.DynamicLinqCore.Test
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             var people = new[] { new Person { Age = 50 }, new Person { Age = 20 } };
             var result = people.AsQueryable().ToDataSourceResult(1, 2, null, null, new[]
